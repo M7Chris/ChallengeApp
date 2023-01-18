bafb4c1575d6fb4f6ed4561e1fb02cd12f822338
Chris <kmjw@protonmail.com>
2023-01-18 23:30:02 +0100

    day 4 homework

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Chris!");
+﻿//day 4 homework
+
+var name = "Eve";
+var sex = "Female";
+var age = 33;
+
+if (sex == "Female" && age == 33)
+{
+    if (name == "Eve")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else
+    {
+        Console.WriteLine("ma 33, lata, ale to nie Ewa");
+    }
+}
+else if (sex == "Female" && age != 33)
+{
+    Console.WriteLine("To kobieta i jest w inym wieku niz 33");
+}
+else if (sex == "Male" && age < 18)
+{
+    Console.WriteLine("UWAGA: niepelnoletni");
+}
+else
+    Console.WriteLine("to mezczyzna");
