4a3542bc4c01786883ee3ee6c03915367ce61fac
Chris <kmjw@protonmail.com>
2023-01-24 13:09:51 +0100

    homework day5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,27 +1,82 @@
-﻿//day 4 homework
+﻿//day 5
 
-var name = "Eve";
-var sex = "Female";
-var age = 33;
+//pierwszy sposob deklarowania tablic
+/*
+int[] grades = new int[5];
+string[] dayOfWeeks = new string[7];
+dayOfWeeks[0] = "monday";
+dayOfWeeks[1] = "tuesday";
+dayOfWeeks[2] = "wednesday";
+dayOfWeeks[3] = "thursday";
+dayOfWeeks[4] = "friday";
+dayOfWeeks[5] = "saturday";
+dayOfWeeks[6] = "sunday";
+*/
 
-if (sex == "Female" && age == 33)
+//drugi sposob deklarowania tablic
+//string[] dayOfWeeks2 = {"monday","tuesday","wednesday","thursday","friday","saturday","sunday"};
+//Console.WriteLine(dayOfWeeks2[3]);
+
+//petla for
+/*
+for(var i=0; i<dayOfWeeks.Length; i++)
 {
-    if (name == "Eve")
+    Console.WriteLine(dayOfWeeks[i]);
+}
+*/
+
+//Listy
+/*
+List<string> dayOfWeeks = new List<string>();
+dayOfWeeks.Add("monday");
+dayOfWeeks.Add("tuesday");
+dayOfWeeks.Add("wednesday");
+dayOfWeeks.Add("thursday");
+dayOfWeeks.Add("friday");
+dayOfWeeks.Add("saturday");
+dayOfWeeks.Add("sunday");
+*/
+/*
+for (var i = 0; i < dayOfWeeks.Count; i++)
+{
+    Console.WriteLine(dayOfWeeks[i]);
+}
+*/
+
+//petla foreach
+/*
+foreach(var day in dayOfWeeks)
+{
+    Console.WriteLine(day);
+}
+*/
+
+//ile cyfr wystepuje w danej liczbie?
+
+int number = 12797;
+string numberAsString = number.ToString(); //zamiana liczby na string
+char[] digits = numberAsString.ToCharArray(); //zamiana string na tablice char
+
+List<char> digit = new List<char>();
+
+digit.Add('0');
+digit.Add('1');
+digit.Add('2');
+digit.Add('3');
+digit.Add('4');
+digit.Add('5');
+digit.Add('6');
+digit.Add('7');
+digit.Add('8');
+digit.Add('9');
+
+
+foreach (var isDigit in digit)
+{
+    int count = 0;
+    foreach (var i in digits)
     {
-        Console.WriteLine("Ewa, lat 33");
+        if (isDigit == i) count++;
     }
-    else
-    {
-        Console.WriteLine("ma 33, lata, ale to nie Ewa");
-    }
+    Console.WriteLine(isDigit + " ===> " + count);
 }
-else if (sex == "Female" && age != 33)
-{
-    Console.WriteLine("To kobieta i jest w inym wieku niz 33");
-}
-else if (sex == "Male" && age < 18)
-{
-    Console.WriteLine("UWAGA: niepelnoletni");
-}
-else
-    Console.WriteLine("to mezczyzna");
