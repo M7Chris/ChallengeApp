db620bc076c699c1aa247a9799b92a59db346471
Chris <kmjw@protonmail.com>
2023-01-25 12:40:34 +0100

    day 5 ze wskazowka od Adama

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -58,6 +58,76 @@ string numberAsString = number.ToString(); //zamiana liczby na string
 char[] digits = numberAsString.ToCharArray(); //zamiana string na tablice char
 List<char> numbers = new List<char>();
 
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char d in digits)
+{
+    if (d == '0')
+    {
+        counter0++;
+    }
+    else if (d == '1')
+    {
+        counter1++;
+    }
+    else if (d == '2')
+    {
+        counter2++;
+    }
+    else if (d == '3')
+    {
+        counter3++;
+    }
+    else if (d == '4')
+    {
+        counter4++;
+    }
+    else if (d == '5')
+    {
+        counter5++;
+    }
+    else if (d == '6')
+    {
+        counter6++;
+    }
+    else if (d == '7')
+    {
+        counter7++;
+    }
+    else if (d == '8')
+    {
+        counter8++;
+    }
+    else if (d == '9')
+    {
+        counter9++;
+    }
+   
+    {
+        Console.WriteLine(digits);
+        Console.WriteLine($"0 pojawia się {counter0} raz/y");
+        Console.WriteLine($"1 pojawia się {counter1} raz/y");
+        Console.WriteLine($"2 pojawia się {counter2} raz/y");
+        Console.WriteLine($"3 pojawia się {counter3} raz/y");
+        Console.WriteLine($"4 pojawia się {counter4} raz/y");
+        Console.WriteLine($"5 pojawia się {counter5} raz/y");
+        Console.WriteLine($"6 pojawia się {counter6} raz/y");
+        Console.WriteLine($"7 pojawia się {counter7} raz/y");
+        Console.WriteLine($"8 pojawia się {counter8} raz/y");
+        Console.WriteLine($"9 pojawia się {counter9} raz/y");
+    }
+}
+
+/*
 numbers.Add('0');
 numbers.Add('1');
 numbers.Add('2');
@@ -79,6 +149,7 @@ foreach (var digit in numbers)
     }
     Console.WriteLine(digit + " ===> " + count);
 }
+*/
 
 /*
  //albo for zamiast foreach
